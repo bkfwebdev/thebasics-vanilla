4c664e605d9d00976b5172642590b9226a07fce0
Bryant Feld <bryantfeld@Bryants-MacBook-Pro.local>
2020-02-04 08:10:24 -0500

    first commit

---
+++ thebasics.js
@@ -0,0 +1,19 @@
+const theBasics = { html : "https://www.youtube.com/embed/UB1O30fR-EE",
+                        css : "https://www.youtube.com/embed/yfoY53QXEnI",
+                        javascript : "https://www.youtube.com/embed/hdI2bqOjy3c" }; 
+    const htmlbutton = document.getElementById("basic1");
+    const cssbutton = document.getElementById("basic2");
+    const jsbutton = document.getElementById("basic3");
+    const videocontent = document.getElementById("video-content")
+    htmlbutton.addEventListener("click",()=>{
+        videocontent.setAttribute("src",theBasics.html);
+        console.log("html");
+    });
+    cssbutton.addEventListener("click",()=>{
+        videocontent.setAttribute("src",theBasics.css);
+        console.log("css");
+    });
+    jsbutton.addEventListener("click",()=>{
+        videocontent.setAttribute("src",theBasics.javascript);
+        console.log("javascript");
+    });
